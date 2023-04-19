79ad7c3a50d97df61443ee212c11d0ae9dd8ec83
KillerCatLion <82245879+KillerCatLion@users.noreply.github.com>
2023-04-19 16:25:45 -0400

    Continuing VPN app

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 //An index of pages
-const appIndex {
-    "vpn": undefined
+var appIndex = {
+    "vpn": 0
 }
 //Decide which direction to slide the home div, and how much. c is current and t is target.
 const pageOffsets = [
@@ -40,7 +40,7 @@ document.addEventListener("load", function() {
         });
     });
 
-    document.querySelector(".icon").forEach(iconBtn => {
+    document.querySelectorAll(".icon").forEach(iconBtn => {
         var iconButton = document.getElementById(iconBtn.id);
         iconButton.addEventListener("click", function() {
             var app = iconBtn.id;
@@ -56,27 +56,37 @@ setTimeout(function() {
     document.dispatchEvent(loadevent);
 }, 1000)
 
-class neth {
+class nethandler {
     get(url, data, options) {
         return new Promise(async function(resolve, reject) {
             var XHR = new XMLHttpRequest();
             XHR.addEventListener("load", function(event) {
                 resolve(event);
+                XHR = null;
             });
             XHR.addEventListener("abort", function(event) {
                 reject(event);
+                XHR = null;
             });
             XHR.addEventListener("error", function(event) {
                 reject(event);
+                XHR = null;
             });
-            
+            XHR.open("get", url);
+            XHR.send();
         });
     }
 }
+const neth = new nethandler;
 
 class vpnApp {
     constructor() {
-
+        console.log("Fetching...")
+        neth.get("https://proxylist.geonode.com/api/proxy-list?limit=250&page=1&sort_by=responseTime&sort_type=asc&filterUpTime=90&protocols=socks5&anonymityLevel=elite&anonymityLevel=anonymous").then(respose => {
+            console.log(respose);
+        });
+    }
+    connect() {
         this.proxyConfig = {
             "mode": "fixed_servers",
             "rules": {
@@ -87,10 +97,11 @@ class vpnApp {
             }
         }
     }
-    connect() {
-
-    }
     disconnect() {
 
     }
+    open() {
+
+    }
 }
+appIndex.vpn = new vpnApp();
